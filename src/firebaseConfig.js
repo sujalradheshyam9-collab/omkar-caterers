80c34719218c081dfe644690d2b786eb7506881f
sujalradheshyam9-collab <sujalradheshyam9@gmail.com>
2026-08-09 19:33:09 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC-T8BR_n0pKNqN_MXqy1HUi5g7wVr73Ww",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
